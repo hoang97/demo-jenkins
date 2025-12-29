b6bc411e5bd2b28129b8cf9e8031d2a21b004131
hoang <hoang>
2025-12-29 15:15:40 +0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  res.end('Hello Jenkins CI/CD! webhook ok 1\n');
+  res.end('Hello Jenkins CI/CD! webhook ok 2\n');
 });
 
 server.listen(port, () => {
